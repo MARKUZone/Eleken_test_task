1591d3e6d9c97d11dfbaef8ae5ad3b897ce71a5c
Nikita Volkov <2545442@gmail.com>
2018-03-17 01:05:40 +0200

    Appearance edit #7

--- dist/js/actions-jquery.js
+++ dist/js/actions-jquery.js
@@ -1,14 +1,42 @@
 // test
-$( "#clickme" ).click(function() {
+/*$( "#clickme" ).click(function() {
 	$( "#book" ).slideToggle( "slow", function() {
 	});
+});*/
+$( "#clickme" ).click(function() {
+	$( ".xslider" ).animate({
+		opacity: 1,
+		left: "-=2400"
+		//display: "block"
+		//height: "toggle"
+	}, 1000, function() {
+		// Animation complete.
+	});
+});
+function xslider(item) {
+	$( item ).animate({
+		opacity: 1,
+		left: "-=2400"
+	}, 1000, function() {
+		// Animation complete.
+	});
+}
+$( document ).ready(function() {
+	const blocks = 3;
+	for ( let i = 1; i <= blocks; ++i ) {
+		const item = ".xslider" + i;
+		const delay = i*350;
+		console.log(item, delay);
+		setTimeout( () => xslider(item), delay);
+	}
 });
 
 
 
 // reveal additional info at About blocks and rotate its arrow on click
-$( "#click-field-1" ).click(function() {
-	$( "#hide-field-1" ).slideToggle( 500, function() {
+$( ".click-field" ).click(function() {
+	console.log(this);
+	$( ".hide-block" ).slideToggle( 500, function() {
 	});
 	if ($( ".fa-angle-down" ).attr("aria-hidden") == "true" ) {
 		$( ".fa-angle-down" ).css({"transform": "rotate(180deg)"});
