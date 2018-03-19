84d6e7007dd6fa0c6731bf7aeb06dada3bb962cd
Nikita Volkov <2545442@gmail.com>
2018-03-19 22:33:38 +0200

    Appearance edit #10 - same problem

--- dist/js/about.js
+++ dist/js/about.js
@@ -3,208 +3,235 @@
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
-var About = function About() {
-	return React.createElement(
-		"div",
-		{ className: "About-comp" },
-		React.createElement(
-			"section",
-			{ className: "about xslider-container" },
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var About = function (_React$Component) {
+	_inherits(About, _React$Component);
+
+	function About() {
+		_classCallCheck(this, About);
+
+		return _possibleConstructorReturn(this, (About.__proto__ || Object.getPrototypeOf(About)).apply(this, arguments));
+	}
+
+	_createClass(About, [{
+		key: "componentDidMount",
+		value: function componentDidMount() {
+			aboutRevealer();
+		}
+	}, {
+		key: "render",
+		value: function render() {
 			React.createElement(
 				"div",
-				{ className: "container" },
+				{ className: "About-comp" },
 				React.createElement(
-					"div",
-					{ className: "row m0" },
+					"section",
+					{ className: "about xslider-container" },
 					React.createElement(
 						"div",
-						{ className: "col" },
+						{ className: "container" },
 						React.createElement(
-							"h2",
-							{ className: "xslider xslider1" },
-							"Calendar date range finder app"
-						),
-						React.createElement(
-							"ul",
-							null,
+							"div",
+							{ className: "row m0" },
 							React.createElement(
-								"li",
-								{ className: "xslider xslider2" },
+								"div",
+								{ className: "col" },
 								React.createElement(
-									"div",
-									{ className: "row shadow-aim click-field" },
+									"h2",
+									{ className: "xslider xslider1" },
+									"Calendar date range finder app"
+								),
+								React.createElement(
+									"ul",
+									null,
 									React.createElement(
-										"div",
-										{ className: "col-1 dflex" },
-										React.createElement("i", { className: "fas fa-clipboard-list" })
-									),
-									React.createElement(
-										"div",
-										{ className: "col" },
-										React.createElement(
-											"a",
-											{ href: "#", className: "dflex" },
-											React.createElement(
-												"h4",
-												null,
-												"How to Setup"
-											),
-											React.createElement("i", { className: "fas fa-angle-down" })
-										),
+										"li",
+										{ className: "xslider xslider2" },
 										React.createElement(
 											"div",
-											{ className: "hide-block" },
+											{ className: "row shadow-aim click-field" },
 											React.createElement(
-												"p",
-												null,
-												React.createElement(
-													"b",
-													null,
-													"Step 1:"
-												),
-												" git clone this repo"
+												"div",
+												{ className: "col-1 dflex" },
+												React.createElement("i", { className: "fas fa-clipboard-list" })
 											),
 											React.createElement(
-												"p",
-												null,
+												"div",
+												{ className: "col" },
 												React.createElement(
-													"b",
-													null,
-													"Step 2:"
+													"a",
+													{ href: "#", className: "dflex" },
+													React.createElement(
+														"h4",
+														null,
+														"How to Setup"
+													),
+													React.createElement("i", { className: "fas fa-angle-down" })
 												),
-												" cd to the cloned"
-											),
-											React.createElement(
-												"p",
-												null,
 												React.createElement(
-													"b",
-													null,
-													"Step 3:"
-												),
-												" install the Application"
+													"div",
+													{ className: "hide-block" },
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 1:"
+														),
+														" git clone this repo"
+													),
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 2:"
+														),
+														" cd to the cloned"
+													),
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 3:"
+														),
+														" install the Application"
+													)
+												)
 											)
 										)
-									)
-								)
-							),
-							React.createElement(
-								"li",
-								{ className: "xslider xslider3" },
-								React.createElement(
-									"div",
-									{ className: "row shadow-aim click-field" },
-									React.createElement(
-										"div",
-										{ className: "col-1 dflex" },
-										React.createElement("i", { className: "fas fa-clipboard-list" })
 									),
 									React.createElement(
-										"div",
-										{ className: "col" },
-										React.createElement(
-											"a",
-											{ href: "#", className: "dflex" },
-											React.createElement(
-												"h4",
-												null,
-												"How to Setup"
-											),
-											React.createElement("i", { className: "fas fa-angle-down" })
-										),
+										"li",
+										{ className: "xslider xslider3" },
 										React.createElement(
 											"div",
-											{ className: "hide-block" },
+											{ className: "row shadow-aim click-field" },
 											React.createElement(
-												"p",
-												null,
-												React.createElement(
-													"b",
-													null,
-													"Step 1:"
-												),
-												" git clone this repo"
+												"div",
+												{ className: "col-1 dflex" },
+												React.createElement("i", { className: "fas fa-clipboard-list" })
 											),
 											React.createElement(
-												"p",
-												null,
+												"div",
+												{ className: "col" },
 												React.createElement(
-													"b",
-													null,
-													"Step 2:"
+													"a",
+													{ href: "#", className: "dflex" },
+													React.createElement(
+														"h4",
+														null,
+														"How to Setup"
+													),
+													React.createElement("i", { className: "fas fa-angle-down" })
 												),
-												" cd to the cloned"
-											),
-											React.createElement(
-												"p",
-												null,
 												React.createElement(
-													"b",
-													null,
-													"Step 3:"
-												),
-												" install the Application"
+													"div",
+													{ className: "hide-block" },
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 1:"
+														),
+														" git clone this repo"
+													),
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 2:"
+														),
+														" cd to the cloned"
+													),
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 3:"
+														),
+														" install the Application"
+													)
+												)
 											)
 										)
-									)
-								)
-							),
-							React.createElement(
-								"li",
-								{ className: "xslider xslider4" },
-								React.createElement(
-									"div",
-									{ className: "row shadow-aim click-field" },
-									React.createElement(
-										"div",
-										{ className: "col-1 dflex" },
-										React.createElement("i", { className: "fas fa-clipboard-list" })
 									),
 									React.createElement(
-										"div",
-										{ className: "col" },
-										React.createElement(
-											"a",
-											{ href: "#", className: "dflex" },
-											React.createElement(
-												"h4",
-												null,
-												"How to Setup"
-											),
-											React.createElement("i", { className: "fas fa-angle-down" })
-										),
+										"li",
+										{ className: "xslider xslider4" },
 										React.createElement(
 											"div",
-											{ className: "hide-block" },
+											{ className: "row shadow-aim click-field" },
 											React.createElement(
-												"p",
-												null,
-												React.createElement(
-													"b",
-													null,
-													"Step 1:"
-												),
-												" git clone this repo"
+												"div",
+												{ className: "col-1 dflex" },
+												React.createElement("i", { className: "fas fa-clipboard-list" })
 											),
 											React.createElement(
-												"p",
-												null,
+												"div",
+												{ className: "col" },
 												React.createElement(
-													"b",
-													null,
-													"Step 2:"
+													"a",
+													{ href: "#", className: "dflex" },
+													React.createElement(
+														"h4",
+														null,
+														"How to Setup"
+													),
+													React.createElement("i", { className: "fas fa-angle-down" })
 												),
-												" cd to the cloned"
-											),
-											React.createElement(
-												"p",
-												null,
 												React.createElement(
-													"b",
-													null,
-													"Step 3:"
-												),
-												" install the Application"
+													"div",
+													{ className: "hide-block" },
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 1:"
+														),
+														" git clone this repo"
+													),
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 2:"
+														),
+														" cd to the cloned"
+													),
+													React.createElement(
+														"p",
+														null,
+														React.createElement(
+															"b",
+															null,
+															"Step 3:"
+														),
+														" install the Application"
+													)
+												)
 											)
 										)
 									)
@@ -213,13 +240,11 @@ var About = function About() {
 						)
 					)
 				)
-			)
-		),
-		aboutRevealer()
-	);
-};
+			);
+		}
+	}]);
 
-//aboutRevealer();
-
+	return About;
+}(React.Component);
 
 exports.default = About;
--- src/jsx/about.jsx
+++ src/jsx/about.jsx
@@ -1,75 +1,79 @@
-const About = () => (
-	<div className="About-comp">
-		<section className="about xslider-container">
-			<div className="container">
-				<div className="row m0">
-					<div className="col">
-						<h2 className="xslider xslider1">Calendar date range finder app</h2>
-						<ul>
-							<li className="xslider xslider2">
-								<div className="row shadow-aim click-field">
-									<div className="col-1 dflex">
-										<i className="fas fa-clipboard-list"></i>
-									</div>
-									<div className="col">
-										<a href="#" className="dflex">
-											<h4>How to Setup</h4>
-											<i className="fas fa-angle-down"></i>
-										</a>
-										<div className="hide-block">
-											<p><b>Step 1:</b> git clone this repo</p>
-											<p><b>Step 2:</b> cd to the cloned</p>
-											<p><b>Step 3:</b> install the Application</p>
+class About extends React.Component {
+	
+	componentDidMount() {
+		aboutRevealer();
+	}
+
+	render () {
+		<div className="About-comp">
+			<section className="about xslider-container">
+				<div className="container">
+					<div className="row m0">
+						<div className="col">
+							<h2 className="xslider xslider1">Calendar date range finder app</h2>
+							<ul>
+								<li className="xslider xslider2">
+									<div className="row shadow-aim click-field">
+										<div className="col-1 dflex">
+											<i className="fas fa-clipboard-list"></i>
+										</div>
+										<div className="col">
+											<a href="#" className="dflex">
+												<h4>How to Setup</h4>
+												<i className="fas fa-angle-down"></i>
+											</a>
+											<div className="hide-block">
+												<p><b>Step 1:</b> git clone this repo</p>
+												<p><b>Step 2:</b> cd to the cloned</p>
+												<p><b>Step 3:</b> install the Application</p>
+											</div>
 										</div>
 									</div>
-								</div>
-							</li>
-							<li className="xslider xslider3">
-								<div className="row shadow-aim click-field">
-									<div className="col-1 dflex">
-										<i className="fas fa-clipboard-list"></i>
-									</div>
-									<div className="col">
-										<a href="#" className="dflex">
-											<h4>How to Setup</h4>
-											<i className="fas fa-angle-down"></i>
-										</a>
-										<div className="hide-block">
-											<p><b>Step 1:</b> git clone this repo</p>
-											<p><b>Step 2:</b> cd to the cloned</p>
-											<p><b>Step 3:</b> install the Application</p>
+								</li>
+								<li className="xslider xslider3">
+									<div className="row shadow-aim click-field">
+										<div className="col-1 dflex">
+											<i className="fas fa-clipboard-list"></i>
+										</div>
+										<div className="col">
+											<a href="#" className="dflex">
+												<h4>How to Setup</h4>
+												<i className="fas fa-angle-down"></i>
+											</a>
+											<div className="hide-block">
+												<p><b>Step 1:</b> git clone this repo</p>
+												<p><b>Step 2:</b> cd to the cloned</p>
+												<p><b>Step 3:</b> install the Application</p>
+											</div>
 										</div>
 									</div>
-								</div>
-							</li>
-							<li className="xslider xslider4">
-								<div className="row shadow-aim click-field">
-									<div className="col-1 dflex">
-										<i className="fas fa-clipboard-list"></i>
-									</div>
-									<div className="col">
-										<a href="#" className="dflex">
-											<h4>How to Setup</h4>
-											<i className="fas fa-angle-down"></i>
-										</a>
-										<div className="hide-block">
-											<p><b>Step 1:</b> git clone this repo</p>
-											<p><b>Step 2:</b> cd to the cloned</p>
-											<p><b>Step 3:</b> install the Application</p>
+								</li>
+								<li className="xslider xslider4">
+									<div className="row shadow-aim click-field">
+										<div className="col-1 dflex">
+											<i className="fas fa-clipboard-list"></i>
+										</div>
+										<div className="col">
+											<a href="#" className="dflex">
+												<h4>How to Setup</h4>
+												<i className="fas fa-angle-down"></i>
+											</a>
+											<div className="hide-block">
+												<p><b>Step 1:</b> git clone this repo</p>
+												<p><b>Step 2:</b> cd to the cloned</p>
+												<p><b>Step 3:</b> install the Application</p>
+											</div>
 										</div>
 									</div>
-								</div>
-							</li>
-						</ul>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-		{aboutRevealer()}
-	</div>
-)
-
-//aboutRevealer();
-
+			</section>
+		</div>
+	}
+}
+	
 
 export default About;
