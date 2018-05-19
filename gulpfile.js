2617b9ff2d5904b8ce50aacbe96ed8459030e62e
Nikita Volkov <2545442@gmail.com>
2018-05-19 11:21:02 +0300

    edit gulpfile.js for helping side project with scss to css convert with watching option

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 
 
+// Test task
 gulp.task('test', () => {
 	console.log("Gulp working!");
 	console.log("Celebrating!");
 });
 
 
+// Run gulp sass for home project
 gulp.task('sass', () => {
 	gulp.src('./src/scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
@@ -15,6 +17,17 @@ gulp.task('sass', () => {
 });
 gulp.task('sass:watch', () => {
 	gulp.watch('./src/scss/*.scss', ['sass']);
+});
+
+
+// Run gulp sass for Admixer project
+gulp.task('sass-admixer', () => {
+	gulp.src('../15_Admixer/Test Front-end/css_test/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('../15_Admixer/Test Front-end/css_test'));
+});
+gulp.task('sass:watch-admixer', () => {
+	gulp.watch('../15_Admixer/Test Front-end/css_test/*.scss', ['sass-admixer']);
 });
 
 
